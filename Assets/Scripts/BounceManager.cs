25cbebd28919413d39e9451cf68d6c2e4471b248
Timothé VANMANSART <timothe.vanmansart@viacesi.fr>
2022-09-27 10:54:51 +0200

    sounds

--- Assets/Scripts/BounceManager.cs
+++ Assets/Scripts/BounceManager.cs
@@ -7,6 +7,10 @@ public class BounceManager : MonoBehaviour
 	private Rigidbody rb;
 	private int currentPosition;
 	private Vector3 bounceForce;
+	public AudioClip[] bounceSounds;
+	public AudioClip pickupSound;
+	public AudioSource audioSource;
+	public AudioListener audioListener;
 	private Vector3[] positions = new Vector3[9] { new Vector3(-45f, 0.1f, -10f), new Vector3(-77f, 0.1f, -34f), new Vector3(20f, 0.1f, -34f), new Vector3(75f, 0.1f, -8f), new Vector3(62.5f, 0.1f, -2f), new Vector3(55f, 0.1f, 43.5f), new Vector3(50f, 0.1f, -63f), new Vector3(-96f, 0.1f, -95f), new Vector3(-60f, 0.1f, -55f) };
 	public GameObject bananaPrefab;
 
@@ -21,10 +25,13 @@ public class BounceManager : MonoBehaviour
 	{
 		if (other.gameObject.CompareTag("BouncyObstacle"))
 		{
+			PlayRandom();
 			bounceForce = new Vector3(Random.Range(-1000.0f, 1000.0f), Random.Range(0f, 100.0f), Random.Range(-1000.0f, 1000.0f));
 			rb.AddForce(bounceForce);
 		} else if (other.gameObject.CompareTag("Pickable"))
         {
+			audioSource.clip = pickupSound;
+			audioSource.Play();
 			currentPosition += 1;
 			if (currentPosition == 9)
             {
@@ -36,7 +43,11 @@ public class BounceManager : MonoBehaviour
 			GameObject.Find("ScoreText").GetComponent<ScoreManager>().score += 1;
 		}
 	}
-
+	void PlayRandom()
+    {
+		audioSource.clip = bounceSounds[Random.Range(0, bounceSounds.Length)];
+		audioSource.Play();
+    }
 	void OnTriggerStay(Collider other)
 	{
 		if (other.gameObject.CompareTag("BouncyObstacle"))
