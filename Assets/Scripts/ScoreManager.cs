c2facc7a3e42af52af09f719c0c3d4f5d2a457e2
Timothé VANMANSART <timothe.vanmansart@viacesi.fr>
2022-09-27 11:18:52 +0200

    high score

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -6,15 +6,25 @@ using TMPro;
 public class ScoreManager : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI highScoreText;
     public int score;
+    public int highscore;
 
     void Start()
     {
         score = 0;
+        highscore = PlayerPrefs.GetInt("highscore", highscore);
+        scoreText.text = highscore.ToString();
     }
 
     void Update()
     {
+        if(score > highscore) {
+            highscore = score;
+            PlayerPrefs.SetInt("High score", highscore);
+            highScoreText.text = "Meilleur Score : " + highscore.ToString();
+        }
         scoreText.text = "Score : " + score.ToString();
+
     }
 }
